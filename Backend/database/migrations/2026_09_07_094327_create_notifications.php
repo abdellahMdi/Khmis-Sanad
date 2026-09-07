26d8b98484acedc05ca70b9fbfa2412d3ca07a55
Abdellahfram <abd20ellah.mah06moudi@gmail.com>
2026-09-07 10:44:40 +0100

    Add migrations for avis and notifications tables

---
+++ Backend/database/migrations/2026_09_07_094327_create_notifications.php
@@ -0,0 +1,25 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type', 255)->nullable();
+            $table->string('notifiable_type', 50)->nullable();
+            $table->dateTime('created_at');
+            $table->dateTime('read_at')->nullable();
+            $table->unsignedInteger('notifiable_id');
+
+            $table->foreign('notifiable_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
